4c662ff1f105254a7d30be07feede100b865e7c5
Karlysson Alves <karlysson.alves@gmail.com>
2017-05-27 11:14:50 +0000

    Testando cookies na pesquisa de produtos na aplicação.

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,6 +1,7 @@
 class Site::SearchController < SiteController
 
   def ads
+    cookies[:search_term] =  params[:q]
     @categories = Category.all
     @ads = Ad.search(params[:q], params[:page])
   end
