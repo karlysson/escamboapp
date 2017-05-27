cde1b456f09402e7f7e201916fb1e2f2f5682f6b
Karlysson Alves <karlysson.alves@gmail.com>
2017-05-27 11:25:17 +0000

    Trabalhando com Session do Rails, coloquei no log da aplicação o nome do Desenvolvedor do Escamboapp.

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -1,5 +1,7 @@
 class Site::HomeController < SiteController
   def index
+    session[:name] = "Desenvolvedor Karlysson Alves"
+    puts ">>>>>>>>>>>>> #{session[:name]}"
     @categories = Category.order_by_description
     @ads = Ad.descending_order(params[:page])
     @carousel = Ad.random(3)
--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -4,5 +4,6 @@ class Site::SearchController < SiteController
     cookies[:search_term] =  params[:q]
     @categories = Category.all
     @ads = Ad.search(params[:q], params[:page])
+    cookies[:categories] = @categories.to_json
   end
 end
